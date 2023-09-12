42b4936d837678214dddf9113a41f4b151212849
Ridley <gonzalezfranks@hotmail.com>
2023-09-13 01:24:36 +0200

    Se añaden más lecciones con un floating action button

--- app/src/main/java/com/jalper/heroesyheroinas/lessonapp/LessonActivity.kt
+++ app/src/main/java/com/jalper/heroesyheroinas/lessonapp/LessonActivity.kt
@@ -1,9 +1,13 @@
 package com.jalper.heroesyheroinas.lessonapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
+import androidx.core.widget.addTextChangedListener
 import com.jalper.heroesyheroinas.R
 import com.jalper.heroesyheroinas.databinding.ActivityLessonBinding
+import com.jalper.heroesyheroinas.databinding.DialogAddLessonBinding
 import com.jalper.heroesyheroinas.lessonapp.model.Language
 import com.jalper.heroesyheroinas.lessonapp.model.Language.Android
 import com.jalper.heroesyheroinas.lessonapp.model.Language.Flutter
@@ -26,6 +30,47 @@ class LessonActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners(){
+        binding.fabLessonsAddButton.setOnClickListener{
+            showDialog()
+        }
+    }
+
+    private fun showDialog(){
+        val dialog = Dialog(this)
+
+        val dialogBinding = DialogAddLessonBinding.inflate(layoutInflater)
+        dialog.setContentView(dialogBinding.root)
+
+        //Desactivamos el boton de añadir nueva leccion si la caja de texto esta vacia
+        dialogBinding.etDialogAddLessonName.addTextChangedListener{ editText ->
+            dialogBinding.btnDialogAddLesson.isEnabled = editText?.isNotBlank() ?: false
+        }
+
+        dialogBinding.btnDialogAddLesson.setOnClickListener{
+            //Obtenemos nombre de la leccion del EditText
+            val name = dialogBinding.etDialogAddLessonName.text.toString()
+            //Obtenemos el id del radio button seleccionada
+            val selectedId = dialogBinding.rgDialogAddLessonLanguage.checkedRadioButtonId
+            val radioButtonSelected = dialogBinding.root.findViewById<RadioButton>(selectedId)
+            val languageSelected = when(radioButtonSelected.text){
+                "Android" -> Android
+                "iOS" -> IOS
+                "Flutter" -> Flutter
+                else -> Android
+            }
+
+            lessonsList.add(Lesson(name, languageSelected))
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+
+    private fun updateLessonsListView(){
+
     }
 
     private fun initUI(){
